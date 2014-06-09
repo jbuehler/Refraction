952c894209b6d887eadabd240b1ba50a2a627d77
Nathan Kluth <nlkluth@gmail.com>
2014-06-09 15:12:16 -0600

    added browserify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,10 +12,19 @@ module.exports = function(grunt) {
 	        keepalive: true
 	      }
 	    }
-		}
+		},
+		browserify: {
+			bundleOptions: {
+				debug: true
+			},
+      build: {
+        src: ['scripts/main.js'],
+        dest: 'dist/js/game.js'
+      }
+    }
 	});
 
 	grunt.registerTask('default', ['serve']);
-	grunt.registerTask('serve', ['connect']);
-  grunt.registerTask('build', ['clean', 'jshint', 'concat', 'uglify', 'copy']); // add these later
+	grunt.registerTask('serve', ['connect', 'browserify']);
+  grunt.registerTask('build', ['clean', 'jshint', 'concat', 'uglify', 'copy']);
 };
