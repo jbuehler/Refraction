3a8ce8f058715482cf45e9e0fcea9320d401a176
Nathan Kluth <nlkluth@gmail.com>
2014-06-12 09:38:13 -0600

    renaming game from copy paste fail :)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,5 @@
 'use strict';
 
 window.onload = function () {
-  var game = new Phaser.Game(288, 505, Phaser.AUTO, 'flappy-bird-reborn');
+  var game = new Phaser.Game(288, 505, Phaser.AUTO, 'refraction');
 };
