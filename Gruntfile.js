df9be74c4e2c10a7da4dde5e775af26141aaf7bb
Nathan Kluth <nlkluth@gmail.com>
2014-06-13 11:41:28 -0600

    comment this out until it's in place

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function(grunt) {
     jshint: {
       options: {
         jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish'),
+        // reporter: require('jshint-stylish'),
         force: false
       },
       all: [
