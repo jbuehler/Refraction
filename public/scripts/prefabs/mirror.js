d34397f1920666b86e7b11a3c6845b4033810fcd
Nathan Kluth <nlkluth@gmail.com>
2014-06-15 12:09:02 -0600

    allow input on mirror

--- public/scripts/prefabs/mirror.js
+++ public/scripts/prefabs/mirror.js
@@ -3,14 +3,17 @@
 var Mirror = function(game, x, y, frame) {
 	Phaser.Sprite.call(this, game, x, y, 'mirror', frame);
 	this.anchor.setTo(0.5, 0.5);
+
+	this.inputEnabled = true;
+	this.events.onInputDown.add(this.rotateMirror, this);
 };
 
 Mirror.prototype = Object.create(Phaser.Sprite.prototype);
 Mirror.prototype.constructor = Mirror;
 
 Phaser.Utils.extend(true, Mirror.prototype, {
-	update: function() {
-
+	rotateMirror: function() {
+		console.log('rotate!');
 	}
 });
 
