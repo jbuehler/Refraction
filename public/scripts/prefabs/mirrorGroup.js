0244117c5dd2a8f25c6d70a8f5ab169848830900
Nathan Kluth <nlkluth@gmail.com>
2014-06-18 22:18:39 -0600

    addmirrors method

--- public/scripts/prefabs/mirrorGroup.js
+++ public/scripts/prefabs/mirrorGroup.js
@@ -9,4 +9,11 @@ var MirrorGroup = function(game, parent) {
 MirrorGroup.prototype = Object.create(Phaser.Group.prototype);
 MirrorGroup.prototype.constructor = MirrorGroup;
 
+Phaser.Utils.extend(true, MirrorGroup.prototype, {
+	rotateMirror: function() {
+		var newAngle = this.angle - 40;
+		this.game.add.tween(this).to({angle: newAngle}, 100).start();
+	}
+});
+
 module.exports = MirrorGroup;
--- public/scripts/states/levels/one.js
+++ public/scripts/states/levels/one.js
@@ -10,5 +10,6 @@ module.exports = {
 
 	setupMirrors: function() {
 		var mirrors = new Mirrors();
+		mirrors.addMirrors();
 	}
 };
