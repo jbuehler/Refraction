19f76fa04cd131bb8ea4a18bdc3c434549447a96
Nathan Kluth <nlkluth@gmail.com>
2014-06-18 22:19:05 -0600

    scratch that

--- public/scripts/prefabs/mirrorGroup.js
+++ public/scripts/prefabs/mirrorGroup.js
@@ -10,10 +10,7 @@ MirrorGroup.prototype = Object.create(Phaser.Group.prototype);
 MirrorGroup.prototype.constructor = MirrorGroup;
 
 Phaser.Utils.extend(true, MirrorGroup.prototype, {
-	rotateMirror: function() {
-		var newAngle = this.angle - 40;
-		this.game.add.tween(this).to({angle: newAngle}, 100).start();
-	}
+
 });
 
 module.exports = MirrorGroup;
