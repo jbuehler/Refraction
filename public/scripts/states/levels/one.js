0107bc13df46ca30cf4b98f0722918ea8a8a80e4
Nathan Kluth <nlkluth@gmail.com>
2014-06-19 23:25:45 -0600

    setup the beam

--- public/scripts/states/levels/one.js
+++ public/scripts/states/levels/one.js
@@ -1,16 +1,24 @@
 'use strict';
 
-var Mirrors = require('../../prefabs/mirrorGroup');
+var Mirrors = require('../../prefabs/mirrorGroup'),
+	Beam = require('../../prefabs/beam');
+
 var mirrorGroup, mirrors;
 
 module.exports = {
 	create: function() {
 		this.setupMirrors();
+		this.setupBeam();
 	},
 
 	setupMirrors: function() {
 		mirrors = this.game.add.group();
 		mirrorGroup = new Mirrors(this.game, mirrors);
 		mirrorGroup.addMirror(100, 100);
+	},
+
+	setupBeam: function() {
+		var beam = new Beam(this.game, this.game.height, this.game.width / 2, this);
+		this.game.add.existing(beam);
 	}
 };
