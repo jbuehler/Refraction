36afa77182e8850772030f1d5549aa0f7dd987e9
Nathan Kluth <nlkluth@gmail.com>
2014-06-23 13:17:39 -0600

    load text

--- public/scripts/states/menu.js
+++ public/scripts/states/menu.js
@@ -4,6 +4,12 @@ module.exports = {
 	create: function() {
 		var startButton = this.game.add.button(this.game.width / 2, 300, 'startButton', this.startClick, this);
     startButton.anchor.setTo(0.5, 0.5);
+
+    var gameText = this.game.add.bitmapText(this.game.width, 180, 'kennyfont', '', 18);
+		this.add(gameText);
+
+		var starText = this.game.add.bitmapText(startButton.width * 0.90, 300, 'kennyfont', '', 18);
+		this.add(starText);
   },
   startClick: function() {
     this.game.state.start('one');
