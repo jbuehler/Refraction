8ab61e4fe4e832c6be9bafb954c1d797984bad0a
Nathan Kluth <nlkluth@gmail.com>
2014-06-23 13:19:36 -0600

    set text

--- public/scripts/states/menu.js
+++ public/scripts/states/menu.js
@@ -5,10 +5,10 @@ module.exports = {
 		var startButton = this.game.add.button(this.game.width / 2, 300, 'startButton', this.startClick, this);
     startButton.anchor.setTo(0.5, 0.5);
 
-    var gameText = this.game.add.bitmapText(this.game.width, 180, 'kennyfont', '', 18);
+    var gameText = this.game.add.bitmapText(this.game.width, 180, 'kennyfont', 'To play, line up mirrors and reflect the light to collect items!', 18);
 		this.add(gameText);
 
-		var starText = this.game.add.bitmapText(startButton.width * 0.90, 300, 'kennyfont', '', 18);
+		var starText = this.game.add.bitmapText(startButton.width * 0.90, 300, 'kennyfont', 'Start', 18);
 		this.add(starText);
   },
   startClick: function() {
