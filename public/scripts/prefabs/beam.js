bd55c36f608b93aeced53569aeec0eebadac7088
Nathan Kluth <nlkluth@gmail.com>
2014-07-02 13:01:13 -0600

    clear bitmap where lines is being drawn

--- public/scripts/prefabs/beam.js
+++ public/scripts/prefabs/beam.js
@@ -1,8 +1,10 @@
 'use strict';
+var _game;
 
 var Beam = function(game, x, y) {
 	Phaser.BitmapData.call(this, game, 'beam', x, y);
 
+	_game = game;
 	this.context.fillStyle = 'rgb(255, 255, 255)';
   this.context.strokeStyle = 'rgb(255, 255, 255)';
 };
@@ -11,6 +13,9 @@ Beam.prototype = Object.create(Phaser.Sprite.prototype);
 Beam.prototype.constructor = Beam;
 
 Phaser.Utils.extend(true, Beam.prototype, {
+	update: function() {
+		this.bitmap.context.clearRect(0, 0, _game.width, _game.height);
+	},
 });
 
 module.exports = Beam;
