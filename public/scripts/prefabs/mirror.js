07263b660f48845fcdd801ef255cdbc248d83e37
Nathan Kluth <nlkluth@gmail.com>
2014-07-13 17:04:22 -0600

    trying this

--- public/scripts/prefabs/mirror.js
+++ public/scripts/prefabs/mirror.js
@@ -1,12 +1,10 @@
 'use strict';
 
+var beam = require('./beam');
+
 var Mirror = function(game, x, y, frame) {
 	Phaser.Sprite.call(this, game, x, y, 'mirror', frame);
 	this.anchor.setTo(0.5, 0.5);
-  this.game.physics.arcade.enableBody(this);
-
-  this.body.allowGravity = false;
-  this.body.immovable = true;
 
 	this.inputEnabled = true;
 	this.events.onInputDown.add(this.rotateMirror, this);
@@ -19,6 +17,12 @@ Phaser.Utils.extend(true, Mirror.prototype, {
 	rotateMirror: function() {
 		var newAngle = this.angle - 40;
 		this.game.add.tween(this).to({angle: newAngle}, 100).start();
+	},
+
+	update: function() {
+		this.game.physics.arcade.collide(this, beam, function() {
+			console.log('test');
+		}, null, this);
 	}
 });
 
