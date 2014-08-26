42e7db54dbb81812d02d4204c7a26eef37d6fb2b
Nathan Kluth <nlkluth@gmail.com>
2014-08-26 11:49:56 -0600

    add prism to scene

--- public/scripts/states/levels/one.js
+++ public/scripts/states/levels/one.js
@@ -1,20 +1,24 @@
 'use strict';
 
 var Mirrors = require('../../prefabs/mirrorGroup'),
-	Beam = require('../../prefabs/beam');
+	Beam = require('../../prefabs/beam'),
+	Prism = require('../../prefabs/prism');
 
-var mirrorGroup, mirrors, beam;
+var mirrorGroup, mirrors, beam, prism;
 
 module.exports = {
 	create: function() {
 		this.setupMirrors();
 		this.setupBeam();
+		this.setupPrism();
 	},
 
 	update: function() {
 		mirrors.forEach(function(mirror) {
 			this.game.physics.arcade.collide(beam, mirror, this.collisionHandler, null, this);
 		}, this);
+
+		this.game.physics.overlap(beam, prism, prism.breakBeam, prism.checkBeamColor, this);
 
 		if (beam.body.touching.left) {
 			console.log('test');
@@ -30,6 +34,11 @@ module.exports = {
 	setupBeam: function() {
 		beam = new Beam(this.game, this, -190, -1120);
 		this.game.add.existing(beam);
+	},
+
+	setupPrism: function() {
+		prism = new Prism(this.game, this, -100, -200);
+		this.game.add.existing(prism);
 	},
 
 	collisionHandler: function(beam, mirror) {
