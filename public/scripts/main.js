61c05df430370c0fd13526eb5934a03b92218838
Nathan Kluth <nlkluth@gmail.com>
2014-08-27 13:31:44 -0600

    canvas to enable fill rect

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.onload = function () {
-  var game = new Phaser.Game(288, 505, Phaser.AUTO, 'refraction');
+  var game = new Phaser.Game(288, 505, Phaser.CANVAS, 'refraction');
 
   game.state.add('boot', require('./states/boot'));
   game.state.add('menu', require('./states/menu'));
