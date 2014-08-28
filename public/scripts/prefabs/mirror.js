a5e38ff65b69a2f86383cf9d9afdf4adc8e2cd8e
Nathan Kluth <nlkluth@gmail.com>
2014-08-28 00:22:16 -0600

    adding bounce to mirror to try and get it bouncing right

--- public/scripts/prefabs/mirror.js
+++ public/scripts/prefabs/mirror.js
@@ -4,6 +4,7 @@ var Mirror = function(game, x, y, frame) {
 	Phaser.Sprite.call(this, game, x, y, 'mirror', frame);
 	this.anchor.setTo(0.5, 0.5);
   game.physics.arcade.enableBody(this);
+  this.body.bounce.set(1);
 
   this.body.allowGravity = false;
   this.body.immovable = true;
