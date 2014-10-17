bc5926ee60365cc910e253b0af5b2e9f157dbfe3
Nathan Kluth <nlkluth@gmail.com>
2014-10-17 08:32:06 -0600

    adding click event to mirror

--- public/scripts/prefabs/mirrorGroup.js
+++ public/scripts/prefabs/mirrorGroup.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var Mirror = require('./mirror'), game;
+var Mirror = require('./mirror'), game, mirrorArray, _game;
 
 var MirrorGroup = function(game, parent) {
   Phaser.Group.call(this, game, parent);
 
-  game = game;
+	this.game.input.onDown.add(this.click, this);
+	game = this.game;
+	mirrorArray = [];
+  _game = game;
 };
 
 MirrorGroup.prototype = Object.create(Phaser.Group.prototype);
@@ -15,6 +18,14 @@ Phaser.Utils.extend(true, MirrorGroup.prototype, {
 	addMirror: function(x, y) {
 		var mirror = new Mirror(this.game, x, y, game);
 		this.add(mirror);
+		mirrorArray.push(mirror);
+	},
+
+	click: function(pointer) {
+		var mirrorClicked = _game.physics.p2.hitTest(pointer.position, mirrorArray);
+		if (mirrorClicked.length === 0) { return; }
+
+		mirrorClicked[0].body.rotateLeft(2);
 	}
 });
 
